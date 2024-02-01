8e4713ecc8eba6adc8e693456e77943c1e0dc8a7
Huw Silverthorn <huw.silverthorn@endava.com>
2024-02-01 13:34:18 +0000

    Adding script to install VS

--- Templates/Customizer/InstallVS.ps1
+++ Templates/Customizer/InstallVS.ps1
@@ -1,1 +1,27 @@
+write-host 'Install VS Pro'
+New-Item -Path "C:\" -Name vstempinstall -ItemType Directory #-ErrorAction SilentlyContinue
+$LocalPath = 'C:\vstempinstall'
+$VSProURL="https://aka.ms/vs/17/release/vs_professional.exe"
+$installerFile="vs_professional.exe"
+$outputPath = $LocalPath + '\' + $installerFile
+Invoke-WebRequest -Uri $VSProURL -OutFile $outputPath
+#set-Location $LocalPath
+#$workloadArgument = @(
+#   
+#    '--add Microsoft.Net.Component.4.7.1.SDK'
+#    '--add Microsoft.VisualStudio.Component.Windows10SDK.17134'
+#    '--add Microsoft.Net.Component.4.7.1.TargetingPack'
+#) 
 
+#$optionsAddLayout          = [string]::Join(" ", $workloadArgument )
+$optionsQuiet              = "--quiet"
+#$optionsLayout             = "--layout $downloadPath"
+#$optionsIncludeRecommended = "--includeRecommended"
+
+$vsOptions = @(
+    $optionsQuiet
+)
+
+Start-Process -FilePath $outputPath -ArgumentList $vsOptions -Wait
+
+Remove-item $LocalPath -Force -Verbose -Recurse
